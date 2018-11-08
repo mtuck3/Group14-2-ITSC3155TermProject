e671936defd24160eccfbe39cc8ed8f93ec7dc5e
Ezra <ejesalv1@uncc.edu>
2018-11-08 15:47:11 +0000

    New changes for Thursday

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,15 @@
 class TasksController < ApplicationController
     def index
+        @tasks = Tasks.all
+    
     end
+    
+    def new 
+
+    end
+    #     def show
+    #     @tasks = tasks.All
+    
+    # end
    
 end
