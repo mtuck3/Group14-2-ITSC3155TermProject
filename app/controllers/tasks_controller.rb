6e7a45ece0e6bc7fa01517a277491592fdef0684
EC2 Default User <ec2-user@ip-172-31-6-254.us-east-2.compute.internal>
2018-11-27 03:02:27 +0000

    Created index, show, new and create methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,25 @@
 class TasksController < ApplicationController
-
-
-    
-    def new 
-
+  def index
+     @tasks = Task.all 
+  end
+   def show
+   @task = Task.find(params[:id])
+   end
+     def new 
+    @task= Task.new
     end
     def create
-        render plain: params[:task].inspect
+        @task=Task.new(task_params)
+        
+        if @task.save
+        redirect_to @task
+        else 
+            render'new'
+            end  
     end
-
-
    
+end
+private
+def task_params
+    params.require(:task).permit(:title, :description, :date)
 end
