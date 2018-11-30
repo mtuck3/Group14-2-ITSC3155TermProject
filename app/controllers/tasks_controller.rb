a16603baf9e394ddda35dc3f499f84e2247cf3f5
Saba Mahmood <smahmoo3@uncc.edu>
2018-11-30 15:29:01 +0000

    Added delete functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,6 +20,13 @@ class TasksController < ApplicationController
             render'new'
             end  
     end
+    def destroy
+       @task = Task.find(params[:id])
+       @task.destroy
+       
+       redirect_to tasks_path
+  
+    end
     def edit
         @task = Task.find(params[:id])
     end
