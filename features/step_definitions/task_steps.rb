4ee72881dc7f72a5ea2213419f013be31e52fdc6
Alaya Ahmed <aahmed18@uncc.edu>
2018-12-02 03:15:26 +0000

    added another test case

--- features/step_definitions/task_steps.rb
+++ features/step_definitions/task_steps.rb
@@ -14,4 +14,11 @@ Then("I shoud see the {string} field") do |field_name|
   expect(page).to have_field(field_name)
 end
 
+ # SECOND SCENARIO #
+ Given("I am on the New Task page") do
+   visit('/task/new')
+ end
 
+ When("I fill {string} in the {string} field") do |page_name|
+   click_link page_name
+ end
