5c060d0ca8cdd5b1083587e9f705ef61a10547f0
Alaya Ahmed <aahmed18@uncc.edu>
2018-12-02 03:13:06 +0000

    Fixed the tests

--- features/step_definitions/task_steps.rb
+++ features/step_definitions/task_steps.rb
@@ -14,19 +14,4 @@ Then("I shoud see the {string} field") do |field_name|
   expect(page).to have_field(field_name)
 end
 
- # SECOND SCENARIO #
- Given("I am on the New Task page") do
-   visit('/task/new')
- end
 
- When("I fill {string} in the {string} field") do |page_name|
-   click_link page_name
- end
-
- Then("I click on the {string} button") do |string|
-   pending # Write code here that turns the phrase above into concrete actions
- end
-
-   Then("I should be on the {string} page") do |string|
-   pending # Write code here that turns the phrase above into concrete actions
-end
